522e11f2b050268b841b7e448a3cd7967602efb2
richard <richard@blockthrough.com>
2018-11-25 03:07:51 -0500

    added css

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import './App.css';
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { pageState: props.enthusiasmLevel || 16 };
+    this.state = { pageState: props.enthusiasmLevel || 14 };
   }
 
   // public onIncrement = () => this.updateEnthusiasm(this.state.pageState + 1);
