afce4d65b96898c05385fef44c91d8322b83b4d4
Riicharr <richard@blockthrough.com>
2018-11-25 02:12:39 -0500

    added ui for home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,13 +1,107 @@
 import * as React from "react"
-
-
+import './Home.css'
 class HomePage extends React.Component {
     render(){
+        
+var circleBar = function() {
+    var can = document.getElementById('canvas'),
+        spanProcent = document.getElementById('procent'),
+         c = can.getContext('2d');
+   
+    var posX = can.width / 2,
+        posY = can.height / 2,
+        fps = 1000 / 200,
+        procent = 0,
+        oneProcent = 360 / 100,
+        result = oneProcent * 64;
+    
+    c.lineCap = 'round';
+    arcMove();
+    
+    function arcMove(){
+      var deegres = 0;
+      var acrInterval = setInterval (function() {
+        deegres += 1;
+        c.clearRect( 0, 0, can.width, can.height );
+        procent = deegres / oneProcent;
+  
+        spanProcent.innerHTML = procent.toFixed();
+  
+        c.beginPath();
+        c.arc( posX, posY, 70, (Math.PI/180) * 270, (Math.PI/180) * (270 + 360) );
+        c.strokeStyle = '#b1b1b1';
+        c.lineWidth = '10';
+        c.stroke();
+  
+        c.beginPath();
+        c.strokeStyle = '#3949AB';
+        c.lineWidth = '10';
+        c.arc( posX, posY, 70, (Math.PI/180) * 270, (Math.PI/180) * (270 + deegres) );
+        c.stroke();
+        if( deegres >= result ) clearInterval(acrInterval);
+      }, fps);
+      
+    }
+    
+    
+  }
         return (
+            
             <div>
-                {window.responsiveVoice.speak("hello world")}
-                incentives
+                {setTimeout(circleBar,20)}
+                {/* {window.responsiveVoice.speak("hello world")} */}
+
+
+                <h4 style={{float:'right'}}>Points Collected</h4>
+                <label style={{float:'left'}} className="switch">
+                    <input type="checkbox" />
+                    <span className="slider round"></span>
+                </label>
+
+                <div id={'button-holder'}>
+                    <button id={'score'}></button>
+                    <button id={'trip'} ></button>
+                    <button id={'user'}></button>
+                </div>
+                <div className="canvas-wrap">
+                    <canvas id="canvas" height="300"></canvas>
+                    <span id="procent"></span>
+                </div>
+                <h4>Overall Points Collected</h4>
+                
+
+                <ul className="list-ic vertical">
+                <h4 style={{float:'right'}}> Last Trip Details</h4>
+
+                    <li>
+                    <span>1</span>
+                    <a href="https://www.google.com/maps?client=ubuntu&channel=fs&q=325+front+street+west&oe=utf-8&um=1&ie=UTF-8&sa=X&ved=0ahUKEwjIwcfP8u7eAhXmy4MKHVP4B98Q_AUIDigB">One Eleven</a>
+                    </li>
+                    <li>
+                    <span>2</span>
+                    <a href="https://www.google.com/maps/place/Finch+Station/@43.6499559,-79.5229331,11z/data=!4m8!1m2!2m1!1sfinch+station!3m4!1s0x882b2d0e11cae715:0x59918ef0fd7ad7fa!8m2!3d43.7812624!4d-79.4150846">Finch Station</a>
+                    </li>
+                    <li>
+                    <span>3</span>
+                    <a href="https://www.google.com/maps/search/hillcrest+mall/@43.8553029,-79.437926,17z/data=!3m1!4b1">Hillcrest Mall</a>
+                    </li>
+                </ul>
+
+                <div className="slidecontainer">
+                    
+                    <p>Focused Driving: 77% <span id="demo"></span></p>
+                    <input type="range" min="1" max="100" value="77" className="sliderLine" id="myRange" />
+                    <p>Speed Control: 90% <span id="demo"></span></p>
+                    <input type="range" min="1" max="100" value="90" className="sliderLine" id="myRange" />
+                    <p>Breaking: 88% <span id="demo"></span></p>
+                    <input type="range" min="1" max="100" value="88" className="sliderLine" id="myRange" />
+                    <p>Acceleration: 91% <span id="demo"></span></p>
+                    <input type="range" min="1" max="100" value="91" className="sliderLine" id="myRange" />
+                </div>
+
             </div>
+
+            
         )
     }
 }
